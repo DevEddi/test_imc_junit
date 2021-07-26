f440597cc898762a316bf7ea9dc47c7b9071966d
Edmilson <scovinowork@gmail.com>
2021-07-26 19:23:35 -0300

    Aplication

--- src/main/java/aplication/Program.java
+++ src/main/java/aplication/Program.java
@@ -1,5 +1,27 @@
 package aplication;
 
+import entities.IMC;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main (String[]args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        IMC imc = new IMC();
+        System.out.println("Digite o seu peso: ");
+        imc.setPeso(sc.nextDouble());
+        System.out.println("Digite o seu altura: ");
+        imc.setAltura(sc.nextDouble());
+        System.out.printf("O seu IMC: %.2f", imc.calculcaImc());
+        System.out.println();
+        System.out.println("Você está com saúde??? ");
+        System.out.println(imc.saudavel());
+
+
+
+        sc.close();
+    }
 
 }
--- src/main/java/entities/IMC.java
+++ src/main/java/entities/IMC.java
@@ -1,4 +1,41 @@
 package entities;
 
 public class IMC {
+    private double altura;
+    private double peso;
+    private double resultado;
+    public double calculcaImc(){
+
+        return  resultado = peso / (altura * altura);
+    }
+
+    public String saudavel(){
+        if (resultado <= 20.7) {
+            return "Abaixo do peso";
+        } else if (resultado <= 26.4) {
+            return "Peso ideal";
+        } else if (resultado <= 27.8) {
+            return "marginalmente acima do peso";
+        } else if (resultado <= 31.1) {
+            return "acima do peso ideal";
+        } else  {
+            return "Obeso";
+        }
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
 }
